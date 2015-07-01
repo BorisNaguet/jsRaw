6c64a6dffa332ebc442c2923bdaa72d6ad5e7937
Boris Naguet <boris.naguet@atos.net>
2015-07-01 18:22:07 +0200

    with the right email...

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
-git config user.email "ci@travis.com"
+git config user.email "boris.naguet@atos.net "
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
